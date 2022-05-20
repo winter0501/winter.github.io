c229768244ea912568d2f5f4e8b2be790246f0da
winter0501 <91250436+winter0501@users.noreply.github.com>
2022-05-20 10:38:27 +0800

    add food fold

---
+++ food/food.js
@@ -0,0 +1,35 @@
+function setup() {
+    initializeFields();
+    createCanvas(400, 300);
+    textSize(40);
+}
+
+var choice;
+
+function draw() {
+    background(color(0xDF, 0xEA, 0x45));
+    fill(color(0x45, 0x6F, 0xEA));
+    if (choice == 0)
+        rect(50, 50, 150, 2);
+    if (choice == 1)
+        rect(50, 100, 150, 2);
+    if (choice == 2)
+        rect(50, 150, 150, 2);
+    if (choice == 3)
+        rect(50, 200, 150, 2);
+    if (choice == 4)
+        rect(50, 250, 150, 2);
+    text("Choice 1", 50, 50);
+    text("Choice 2", 50, 100);
+    text("Choice 3", 50, 150);
+    text("Choice 4", 50, 200);
+    text("Choice 5", 50, 250);
+}
+
+function mousePressed() {
+    choice = int(random(3));
+}
+
+function initializeFields() {
+    choice = -1;
+}
